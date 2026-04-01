83f0949ff16e8abe97c84f69eebddc9e46991fd3
wazzali codes <wazzalicodes01@gmail.com>
2026-04-01 21:13:52 +0400

    variables

--- bin/dart_crash_course.dart
+++ bin/dart_crash_course.dart
@@ -2,4 +2,7 @@ import 'package:dart_crash_course/dart_crash_course.dart' as dart_crash_course;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_crash_course.calculate()}!');
+  var age = 25;
+  print('age = $age');
+  print("Hello, Dart project!");
 }
--- bin/variables_datatypes.dart
+++ bin/variables_datatypes.dart
@@ -1,5 +1,29 @@
+import 'dart:math';
+
 void main(List<String> args) {
-  var age = 25;
-  print('age = $age');
-  print("Hello, Dart!");
+  print('Hello, Dart!');
+  print('Hello, Dart Apprentice Reader');
+
+  print(sin(45 * pi / 180));
+  print(135 * pi / 180);
+  print(sqrt(2));
+  print(max(50, 10));
+  print(min(40, -67));
+  print(max(sqrt(2), pi/2));
+
+  // variables.
+  int number = 15;
+  print('number = $number');
+  number = 23;
+  print('number is now = $number');
+  print('10.isEven => ${10.isEven}');
+  print('3.14159.round() => ${3.14159.round()}');
+
+  num myNumber;
+  myNumber = 35;
+  myNumber = 3.14678;
+  print(myNumber);
+
+  // we have as varaiables var, num annd dynamic.
+
 }
