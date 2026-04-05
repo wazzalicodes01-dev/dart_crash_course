9d694250787fd442c18f6c0f564d8c0bf50d3de6
wazzali codes <wazzalicodes01@gmail.com>
2026-04-05 23:13:06 +0400

    control flow

--- bin/control_flow.dart
+++ bin/control_flow.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 const global = 'Hello, World';
 
 void main(List<String> args) {
@@ -48,7 +50,7 @@ void main(List<String> args) {
 
   print(global);
   print(local);
-  //print(insideIf);  // Not allowed
+  // print(insideIf);  // Not allowed
 
   // Ternary Operator.
   const score = 83;
@@ -56,4 +58,110 @@ void main(List<String> args) {
   print('message = $message');
 
   // Switch statements.
+  const number = 3;
+  switch (number) {
+    case 0:
+      print('Zero');
+      break;
+    case 1:
+      print('One');
+      break;
+    case 2:
+      print('Two');
+      break;
+    case 3:
+      print('Three');
+      break;
+    case 4:
+      print('Four');
+      break;
+    default:
+      print('Something else');
+  }
+
+  // Switching on strings.
+  const weather = 'cloudly';
+  switch (weather) {
+    case 'sunny':
+      print('Put on sunscreen');
+      break;
+    case 'snowy':
+      print('Get your skis');
+      break;
+    case 'cloudly':
+    case 'rainy':
+      print('Bring an umbrella');
+      break;
+    default:
+      print('Im not familiar with that weather!');
+  }
+
+  // Enumerated types.
+  const weatherToday = Weather.cloudy;
+  switch (weatherToday) {
+    case Weather.sunny:
+      print('Put on sunscreen');
+      break;
+    case Weather.snowy:
+      print('Get your skis');
+      break;
+    case Weather.cloudy:
+    case Weather.rainy:
+      print('Bring an umbrella');
+      break;
+  }
+  print('weatherToday = $weatherToday');
+  final index = weatherToday.index;
+  print('index = $index');
+
+  // While loops.
+  var sum = 1;
+  while (sum < 10) {
+    sum += 1;
+    print('sum = $sum');
+  }
+
+  // Do-while loops.
+  sum = 1;
+  do {
+    sum += 1;
+    print('sum = $sum');
+  } while (sum < 10);
+
+  // Breaking out of a loop.
+  sum = 1;
+  while (true) {
+    sum += 4;
+    if (sum > 10) {
+      break;
+    }
+  }
+
+  // A random interlude.
+  final random = Random();
+  while (random.nextInt(6) + 1 != 6) {
+    print('Not a six!');
+  }
+  print('Finally, we got a six!');
+
+  // For loops.
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  // With continue keyword.
+  for (var i = 0; i < 5; i++) {
+    if (i == 2) {
+      continue;
+    }
+    print(i);
+  }
+
+  // For-in loops.
+  const myString = 'I ❤ Dart';
+  for (var codePoint in myString.runes) {
+    print(String.fromCharCode(codePoint));
+  }
 }
+
+enum Weather { sunny, snowy, cloudy, rainy }
